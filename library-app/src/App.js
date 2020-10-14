ae179a429ca09ca59cdfd2c882df3b2513103be0
edohor <edohorvat01@gmail.com>
2020-10-14 18:03:02 +0200

    added default books to local storage

--- library-app/src/App.js
+++ library-app/src/App.js
@@ -5,6 +5,14 @@ import Homepage from './components/Homepage';
 import BookList from './components/Books/BookList';
 import UsersList from './components/Users/UsersList';
 
+let defaultBooks = [];
+let bookObj1 = {id: 0, author: "JK Rowling ", title: "Harry Potter", available: true, rentedTo: []};
+let bookObj2 = {id: 1, author: "George R. R. Martin ", title: "A Game of Thrones", available: true, rentedTo: []};
+let bookObj3 = {id: 2, author: "Dan Brown ", title: "The Da Vinci Code", available: true, rentedTo: []};
+defaultBooks.push(bookObj1,bookObj2,bookObj3);
+
+localStorage.setItem("books", JSON.stringify(defaultBooks));
+
 function App() {
   return (
     <div className="App">
